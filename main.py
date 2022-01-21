91b727be30231fd312013bdad3ebde8d35b23df5
Alessandro Murru <88579269+alpha4041@users.noreply.github.com>
2022-01-21 10:56:22 +0100

    fix x3

--- main.py
+++ main.py
@@ -2,11 +2,13 @@ from pyrogram import Client, filters
 import requests
 import os
 from os import environ
+import json
 
 api_id = int(environ["API_ID"])
 api_hash = environ["API_HASH"]
 bot_token = environ["TOKEN"]
-
+thecatapi = environ["THECATAPI"]
+headers = {'x-api-key': thecatapi}
 bot = Client("catgeneratorbot", api_id, api_hash, bot_token=bot_token)
 
 
@@ -20,21 +22,28 @@ def welcome_message(bot, message):
 def cat_generator(bot, message):
     query = " ".join(message.command[1:])
     r = requests.get(
-        "https://api.thecatapi.com/v1/breeds/search?q=" + query).content
-    breed = r.json()["id"]
-    breedimageurl = requests.get(
-        "https://api.thecatapi.com/images/search?breed_id=" + breed).content
+        "https://api.thecatapi.com/v1/breeds/search?q=" + query, headers=headers).content
+    rdecoded = r.decode()
+    rlist = json.loads(rdecoded)
+    breed = rlist[0]['id']
+    description = rlist[0]['description']
+    wikipediaurl = rlist[0]['wikipedia_url']
     if r.status_code != 200:
         print("Error in Cat API")
         bot.send_message(1833693304, "Error occured in Cat API!")
         bot.reply_text(
             "An error occured in the API, try again later or change your request.")
     else:
-        photo = requests.get(breedimageurl.json()["url"]).content
+        breedimageresponse = requests.get(
+            "https://api.thecatapi.com/v1/images/search?breed_id=" + breed, headers=headers).content
+        breedimageresponsedecoded = breedimageresponse.decode()
+        breedimageresponselist = json.loads(breedimageresponsedecoded)
+        imageurl = breedimageresponselist[0]["url"]
+        photo = requests.get(imageurl).content
         filename = "cat.jpg"
         open(filename, "wb").write(photo)
         message.reply_photo(filename, quote=True,
-                            caption=breedimageurl.json()["description"])
+                            caption=(description + "\n" + "Wikipedia:" + wikipediaurl))
         os.remove(filename)
 
 
